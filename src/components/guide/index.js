4ea6954fbf767befa5674230c6e9de7a4f3124c4
taewookim <ktw4071@gmail.com>
2019-04-09 15:14:35 -0400

    adding all files

--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -1,10 +1,12 @@
 import { transform, i18n, lan } from '../../unit/const'
 import { isMobile } from '../../unit'
+
 export default {
   name: 'Guide',
   data() {
     return {
-      isMobile: isMobile()
+      isMobile: isMobile(),
+      IPAdd: 'BE'
     }
   },
   computed: {
@@ -16,12 +18,58 @@ export default {
       window.location.protocol +
       '//binaryify.github.io/vue-tetris/static/qr.jpeg'
   },
+  created() {
+    
+  },
   mounted() {
     window.addEventListener('resize', this.resize.bind(this), true)
+    // getUserIP(function(ip){
+    //   this.IPAdd = ip
+    //   console.log("ip - " + ip)
+    //   console.log("IPAdd - " + this.IPAdd)
+    // })
   },
   methods: {
     resize() {
       this.isMobile = isMobile()
+    },
+
+    getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+      //compatibility for firefox and chrome
+      var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+      var pc = new myPeerConnection({
+          iceServers: []
+      }),
+      noop = function() {},
+      localIPs = {},
+      ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+      key;
+
+      function iterateIP(ip) {
+          if (!localIPs[ip]) onNewIP(ip);
+          localIPs[ip] = true;
+      }
+
+       //create a bogus data channel
+      pc.createDataChannel("");
+
+      // create offer and set local description
+      pc.createOffer().then(function(sdp) {
+          sdp.sdp.split('\n').forEach(function(line) {
+              if (line.indexOf('candidate') < 0) return;
+              line.match(ipRegex).forEach(iterateIP);
+          });
+          
+          pc.setLocalDescription(sdp, noop, noop);
+      }).catch(function(reason) {
+          // An error occurred, so handle the failure to connect
+      });
+
+      //listen for candidate events
+      pc.onicecandidate = function(ice) {
+          if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+          ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+      };
     }
   }
 }
